6af5933c07e1b9e2fed7881091f4c79621a99b49
yuki-o1o5 <jkksst1712@gmail.com>
2023-01-06 02:23:05 -0800

    create hello fashion website

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+function openGender(evt, genderName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(genderName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
